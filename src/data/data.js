029849d5699fd80d560e746b0f5169287c753b3a
maryna-js <rastargueva.m@gmail.com>
2020-03-15 13:25:26 +0300

    feat: add faker

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+import faker from "faker";
+
+faker.seed(781);
+
+export const makeFaker = (idx) => {
+    return {
+        // "id": faker.random.uuid(),
+        "firstName": faker.name.firstName(),
+        "lastName": faker.name.lastName(),
+        "age": faker.random.number({
+            'min': 25,
+            'max': 53
+        }),
+        "position": faker.name.jobTitle(),
+        "hiredAt": faker.date.past().toLocaleString(),
+        "salary": faker.finance.amount(),
+        "email": faker.internet.email(),
+        // "githubId": faker.internet.email(),
+        // "totalScore": 0,
+        // "locationName": faker.address.city(),
+        // "taskResults": [],
+         "isActive": faker.random.boolean()? 'YES' : 'NO',
+        //  "location": 
+        //     {"city": faker.address.city(), "zipcode": faker.address.zipCode()}
+          
+    };
+};
+
+const data = [...new Array(10)].map((idx) => makeFaker(idx));
+
+export default data;
